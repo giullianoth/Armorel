4528c22ab9ffe54f39e77ed0650ae6ee998cde27
Giulliano <tharsoweb@gmail.com>
2024-09-04 13:43:35 -0300

    página de login

--- assets/scripts/change-pass-input.js
+++ assets/scripts/change-pass-input.js
@@ -1,4 +1,4 @@
-import { getElement, getElements } from "../variables.js"
+import { getElement, getElements } from "./variables.js"
 
 const inputsToChange = getElements(".j_password_label")
 const input = (label) => getElement(".j_password_input", label)
@@ -6,15 +6,20 @@ const icon = (label) => getElement(".j_password_change", label)
 
 inputsToChange.forEach(label => {
     let labelIcon = icon(label)
-    let labelInput = input(label)
 
     labelIcon.addEventListener("click", () => {
-        if (labelInput.getAttribute("type") === "password") {
-            labelInput.setAttribute("type", "text")
+        let labelInput = input(label)
+        let inputLength = labelInput.value.length
+
+        if (labelInput.type === "password") {
+            labelInput.type = "text"
             labelIcon.classList.replace("fa-eye-slash", "fa-eye")
-        } else if (labelInput.getAttribute("type") === "text") {
-            labelInput.setAttribute("type", "password")
+        } else if (labelInput.type === "text") {
+            labelInput.type = "password"
             labelIcon.classList.replace("fa-eye", "fa-eye-slash")
         }
+        
+        labelInput.focus()
+        labelInput.setSelectionRange(inputLength, inputLength)
     })
 })
